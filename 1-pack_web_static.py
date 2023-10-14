c3996c303c289cca9f2fd7e011161476087c1b70
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2023-10-14 19:38:37 +0000

    paterne

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-    generates a .tgz archive from contents of web_static
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
 """
-from fabric.api import local
-from datetime import datetime
 
-
-from fabric.api import local
 from datetime import datetime
+from fabric.api import *
 
 
 def do_pack():
     """
-        compress directory to .tgz archive
+    making an archive on web_static folder
     """
+
     time = datetime.now()
-    time = time.strftime('%Y%m%d%H%M%S')
-    archive_path = 'versions/web_static_' + time + '.tgz'
-
-    local('mkdir -p versions/')
-    result = local('tar -cvzf {} web_static/'.format(archive_path))
-
-    if result.succeeded:
-        return archive_path
-    return None
+    archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
+    else:
+        return None
